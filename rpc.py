37550ceab62798a24a2f232299d67e463d57aee0
ChinmayK0607 <114411195+ChinmayK0607@users.noreply.github.com>
2023-12-10 10:27:19 +0530

    Create rpc.py

---
+++ rpc.py
@@ -0,0 +1,20 @@
+from flask import Flask, Response, request
+from jsonrpcserver import method, Result, Success, dispatch
+import requests
+
+app = Flask(__name__)
+
+@method
+def ping() -> Result:
+    return Success("pong")
+
+@app.route("/", methods=['POST'])
+def index():
+    return Response(
+        dispatch(request.get_data().decode()), content_type="application/json"
+    )
+
+if __name__ == "__main__":
+    app.run()
+
+
